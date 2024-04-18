c2e4791e66f213b0770c80e398bff5bbeb816f6d
sourav majumder <sourav.cse18.duet@gmail.com>
2024-04-18 11:13:11 +0600

    Create total leave for user table

---
+++ models/TotalLeaveOfUser.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+
+const TotalLeaveOfUserSchema = mongoose.Schema(
+    {
+        employeeId: {
+            type: String,
+            ref : "user",
+            required: true 
+        },
+        totalSick : {
+            type : Number,
+            default : 0
+        },
+        totalCasual : {
+            type : Number,
+            default : 0
+        }
+    },{
+        toJSON : {
+            transform : function(doc, ret){
+                delete ret.__v;
+                delete ret.createdAt;
+                delete ret.updatedAt;
+            }
+        },
+        timestamps : true
+});
+
+//@exports
+module.exports = mongoose.model("total-leave-of-user", TotalLeaveOfUserSchema);
--- models/modelExporter.js
+++ models/modelExporter.js
@@ -2,6 +2,7 @@ const User = require("../models/ User");
 const Concern = require("../models/Concern");
 const Leave = require("../models/Leave");
 const LeavePerYear = require("../models/LeavePerYear");
+const TotalLeaveOfUser = require("../models/TotalLeaveOfUser");
 const Department = require("../models/Department");
 const Document = require("../models/Document");
 const Expense = require("../models/Expense");
@@ -10,6 +11,7 @@ module.exports = {  User,
                     Concern,
                     Leave,
                     LeavePerYear,
+                    TotalLeaveOfUser,
                     Department,
                     Document,
                     Expense    
