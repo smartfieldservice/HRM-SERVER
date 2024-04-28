6a3ba02f7262574c63cfa5229bdf4493ba4ae91c
sourav majumder <sourav.cse18.duet@gmail.com>
2024-04-28 16:58:26 +0600

    modify dashboard controller's totalInformation function

--- controllers/dashboardController.js
+++ controllers/dashboardController.js
@@ -1,35 +1,39 @@
 //@external module
 const asyncHandler = require('express-async-handler');
-const { Department, User, Concern } = require('../models/modelExporter');
+
+//@internal module
+const { Department, 
+        User, 
+        Concern } = require('../models/modelExporter');
 
 const totalInformation = asyncHandler( async(req, res) => {
 
     try {
-        
-        let concernId = undefined;
 
-        //@after giving the role then use it as concernId
-        //concernId = req.account.concernId;
+        let role = "hr"; 
+
+        //console.log(req.account);
+        
+        //@after giving route protection 
+        //role = req.account.role;
 
         let departments, employees, concerns;
 
-        if(!concernId){
+        if(role === "hr"){
             //@hr
             departments = await Department.find({ });
             employees = await User.find({ });
             concerns = await Concern.find({ });
-
         }else{
             //branch-hr
-            departments = await Department.find({ concernId });
-            employees = await User.find({ concernId });
+            departments = await Department.find({ concernId : req.account.concernId });
+            employees = await User.find({ concernId : req.account.concernId });
         }
         res.status(200).json({ concern : `${concerns.length} concerns found !`, department : `${departments.length} departments found !`, employee : `${employees.length} employees found !`});
 
     } catch (error) {
         res.status(404).json({ message: error});
     }
-
 });
 
 //@exports
