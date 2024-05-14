8b1713692acaebcde02e90c0b89cb3a3c7aa6a55
sourav majumder <sourav.cse18.duet@gmail.com>
2024-05-14 16:54:58 +0600

    minor change inleave per year controller

--- routes/leavePerYearRoute.js
+++ routes/leavePerYearRoute.js
@@ -3,11 +3,18 @@ const router = require("express").Router();
 
 //@internal module
 const { leavePerYearController } = require("../controllers/controllerExporter");
+const { accountValidation } = require("../middlewares/middlwareExporter");
 
 router
     .route("/")
     //@api/leave-per-year?page=&limit=&sort=
-    .get(leavePerYearController.allLeavePerYear)
+    .get(accountValidation.isLogin,accountValidation.requiredRole(["hr", "branch-hr"]),leavePerYearController.allLeavePerYear)
+
+router
+    .use(accountValidation.isLogin, accountValidation.requiredRole(["hr"]));
+
+router
+    .route("/")
     //@api/leave-per-year
     .post(leavePerYearController.createLeavePerYear)
     //@api/leave-per-year?id=<leave_per_year_id>
