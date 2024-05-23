3f0a4298139b173c980cdead6c451e1f73fe2811
sourav majumder <sourav.cse18.duet@gmail.com>
2024-05-23 14:50:07 +0600

    remove unnecessary files

--- utils/common.js
+++ utils/common.js
@@ -2,10 +2,6 @@
 const path = require("path");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
-const multer = require("multer");
-
-//@internal module
-const { unlinkFile } = require("../middlewares/fileDeleteHandler");
 
 //@function for regular expression string
 const escapeString = function(str){
@@ -60,91 +56,6 @@ const decodeAccount = function(authToken){
     return jwt.decode(authToken);
 }
 
-//@make the files array
-const filesArray = function(allFiles){
-
-    let sobFiles =  [];
-
-    if(allFiles.length > 0){
-        for(let i = 0 ; i < allFiles.length ; i++){
-            sobFiles.push(allFiles[i].filename);
-        }
-    }else{
-        sobFiles.push("File not found !");
-    }
-    return sobFiles;
-}
-
-//@uploader function for file upload
-const uploader = function (subFolderPath, allowedFileType, maxFileSize, maxCount, errorMsg){
-    
-    //File upload folder
-    const mainDirectory = path.resolve (__dirname,"..");
-    const uploadFolder = `${mainDirectory}/public/${subFolderPath}/`;
-
-    //define the storage
-    const storage = multer.diskStorage({
-
-        //define destination
-        destination : (req,file,cb)=>{
-            cb(null,uploadFolder);
-        },
-
-        //define fileName
-        filename:(req,file,cb)=>{
-
-            //extract file extention
-            const fileExtention = path.extname(file.originalname);
-
-            //make a unique file name
-            const fileName = file.originalname.replace(fileExtention,"").split(" ").join("-")+ "-" + Date.now() + fileExtention;
-
-            cb(null,fileName);
-        },
-    });
-
-    const upload = multer({
-        storage: storage,
-        limits: {
-          fileSize: maxFileSize,
-          files : maxCount
-        },
-        fileFilter: (req, file, cb) => {
-          if (allowedFileType.includes(file.mimetype)) {
-            cb(null, true);
-          } else {
-            cb(errorMsg);
-          }
-        },
-      });
-    
-      return upload;
-}
-
-//@function for unlink file from local
-const unlinkFileFromLocal = async(allFiles, subFolder) => {
-    
-    try {
-        
-        if(allFiles[0] !== "File not found !"){
-
-            const promises = allFiles.map((fileName)=>{
-                unlinkFile(fileName,subFolder)
-                .then(res => {
-                    console.log("File deleted successfully !");
-                })
-                .catch(error => {
-                    console.log(error.message)
-                })
-            });
-            return await Promise.all(promises);
-        }   
-        return;
-    } catch (error) {
-        
-    }
-}
-
 //@generate a slug from the given string
 const generateSlug = function(str){
     return str.toLowerCase().replace(/\s+/g, "-");
@@ -158,8 +69,5 @@ module.exports = {  escapeString,
                     generateAuthToken,
                     verifyAuthToken,
                     decodeAccount,
-                    filesArray,
-                    uploader,
-                    unlinkFileFromLocal,
                     generateSlug
                 }
